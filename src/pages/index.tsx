12dcffda077a04f3433f5496116b78f5fb3d259a
root <root@jarvis51ct>
2023-06-15 23:25:45 +0900

    first

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
+        <div className={styles.hoge} />
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
